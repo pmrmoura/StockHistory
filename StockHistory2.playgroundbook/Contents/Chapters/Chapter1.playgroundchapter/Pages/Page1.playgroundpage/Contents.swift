fd4d3113a1e675b3d3246d07668ec0e823aae3a8
Pedro Moura <pmrm@cin.ufpe.br>
2020-03-31 11:34:25 -0300

    Playground Book Structure

---
+++ StockHistory2.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page1.playgroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+ # Introdução
+ 
+ Bem vindo! O Stock History é um playground
+ que tem como objetivo demonstrar como as
+ crises mundiais influenciam na subida ou
+ queda da bolsa de valores.
+ 
+ 
+ # Funcionamento
+ 
+ 
+ Cada botão representa uma crise mundial da história recente. Quando clicados, um gráfico aparecerá, mostrando como a bolsa de valores estava antes daquele período, durante e depois, apresentando também fato histórico sobre a crise.
+ 
+ 
+ # Vamos começar!
+ 
+ 
+ Clique em um dos botões ao lado e comece a experiência do Stock History :)
+ 
+ */
+
+//#-hidden-code
+
+import UIKit
+import PlaygroundSupport
+
+let viewController = IntroViewController()
+
+PlaygroundPage.current.liveView = viewController
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//#-end-hidden-code
